5e5e3e185814f9c670c6208494fc2a845b524943
Yulianna2305 <pryuliana023@gmail.com>
2025-10-29 16:44:55 +0200

    Update test_order_manager.py

--- labs/lab_5/test_order_manager.py
+++ labs/lab_5/test_order_manager.py
@@ -1,7 +1,7 @@
 import pytest
 from order_manager import Order
 
-def test_total_various_sets():
+def test_total_various_sets():      #чи правильний підрахунок загальної суми замовлення
     items = [{"price": 10, "quantity": 2}, {"price": 5, "quantity": 3}]
     assert Order(1, items).total() == 10*2 + 5*3
 
@@ -11,7 +11,7 @@ def test_total_various_sets():
     assert Order(3, items).total() == 2_000_000
 
 
-def test_most_expensive():
+def test_most_expensive():            #чи метод знаходить найдорожчий товар
     items = [{"price": 10}, {"price": 50}, {"price": 30}]
     assert Order(4, items).most_expensive()["price"] == 50
 
@@ -21,7 +21,7 @@ def test_most_expensive():
         Order(6, []).most_expensive()
 
 
-def test_apply_discount_valid_for_all():
+def test_apply_discount_valid_for_all():     #перевіряє чи правильно знижує ціни на 10%
     items = [
         {"price": 100, "quantity": 1},
         {"price": 200, "quantity": 2},
@@ -34,14 +34,14 @@ def test_apply_discount_valid_for_all():
 
 
 @pytest.mark.parametrize("discount", [-10, 101, 999])
-def test_apply_discount_invalid(discount):
+def test_apply_discount_invalid(discount):      #перевіряє, що невірні знижки
     items = [{"price": 50, "quantity": 1}]
     order = Order(8, items)
     with pytest.raises(ValueError, match="Invalid discount"):
         order.apply_discount(discount)
 
 
-def test_apply_discount_extreme():
+def test_apply_discount_extreme():      # чи норм працює при 0% та 100%
     items = [{"price": 100, "quantity": 1}, {"price": 50, "quantity": 2}]
     order = Order(9, items)
 
@@ -52,22 +52,12 @@ def test_apply_discount_extreme():
     assert all(item["price"] == 0 for item in items)
 
 
-def test_repr():
+def test_repr():            #чи повертає коректний текстовий опис 
     items = [{"price": 1, "quantity": 1}]
     order = Order(10, items)
     r = repr(order)
     assert f"<Order {order.id}" in r
     assert f"{len(items)} items" in r
-
-
-
-
-
-
-
-
-
-
 
 
 
